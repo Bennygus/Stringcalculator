3bc8aea6d184d44afbd0501158b3b4a21a33775a
Benny <gatts@live.se>
2021-12-07 22:10:20 +0100

    Adds refactoring the for-each loop with sum and IllegalArgumentException

--- src/main/java/org/example/StringCalc.java
+++ src/main/java/org/example/StringCalc.java
@@ -6,38 +6,49 @@ public class StringCalc {
 
     public static int add(String inputString){
         if (inputString.length()==0)return 0;
-        ArrayList<Integer> negativNumbersList= new ArrayList<>();
+
 
 
         inputString = inputString.replaceAll("//;","");
         inputString = inputString.replaceAll(";",",");
 
-        String[] splitList= inputString.trim().replace("\n",",").split(",");
+        String[] splitNumberList= inputString.trim().replace("\n",",").split(",");
 
 
+        return checkForValidNumbersInStringAndGetSum(splitNumberList);
+    }
+
+
+    private static int checkForValidNumbersInStringAndGetSum(String[] splitNumberList) {
         int sum = 0;
-        for (String numberElement:splitList) {
-            int parsedNumberElement = Integer.parseInt(numberElement);
-            if (parsedNumberElement<0){
-                negativNumbersList.add(parsedNumberElement);
-            }
-            sum+=parsedNumberElement;
+        ArrayList<Integer> negativNumbersList= new ArrayList<>();
 
-
-        }
-
-
-
+        sum = iterateEveryNumberInStringToGetSum(splitNumberList, sum, negativNumbersList);
 
         if (negativNumbersList.size()>0)
             throw new IllegalArgumentException("Negatives not allowed: " + negativNumbersList);
 
-
-
         return sum;
     }
 
+    private static int iterateEveryNumberInStringToGetSum(String[] splitNumberList, int sum, ArrayList<Integer> negativNumbersList) {
+        for (String numberElement: splitNumberList) {
 
+            int valueOfNumberElement = getValueOfStringNumbers(negativNumbersList, numberElement);
+
+
+            sum +=valueOfNumberElement;
+        }
+        return sum;
+    }
+
+    private static int getValueOfStringNumbers(ArrayList<Integer> negativNumbersList, String numberElement) {
+        int valueOfNumberElement = Integer.parseInt(numberElement);
+        if (valueOfNumberElement<0){
+            negativNumbersList.add(valueOfNumberElement);
+        }
+        return valueOfNumberElement;
+    }
 
 
 }
